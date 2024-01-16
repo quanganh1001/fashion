40521df0d836c9b64f4ebfe20bd77a650e05d406
quang anh <quanganh@Macbook-Pro-ca-Quang-Anh.local>
2024-01-16 18:16:15 +0700

    16/1 18:16

--- src/main/java/project/fashion/controllerWeb/CtlCart.java
+++ src/main/java/project/fashion/controllerWeb/CtlCart.java
@@ -16,6 +16,7 @@ import project.fashion.model.service.*;
 import java.net.MalformedURLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @Controller
 @SessionAttributes("CARTS")
@@ -53,21 +54,51 @@ public class CtlCart {
         ProductDetail productDetail = productDetailService.getById(prDetailId);
         Product product = productService.findById(productDetail.getProduct().getProductId());
 
-        CartItem cartItem = new CartItem
-                        (productDetail.getCode(),
-                        product.getProductId(),
-                        quantity,
-                        product.getPrice(),
-                        product.getDiscountPrice(),
-                        product.getProductName(),
-                        productDetail.getColor().getName(),
-                        productDetail.getSize().getName()
-                        );
+        boolean itemExists = false;
+        for (CartItem cartItem : cartItemList) {
+            if (cartItem.getCode().equals(productDetail.getCode())) {
+                cartItem.setQuantity(cartItem.getQuantity() + quantity);
+                itemExists = true;
+                break;
+            }
+        }
 
-        cartItemList.add(cartItem);
+        if (!itemExists) {
+            CartItem cartItem = new CartItem(
+                    productDetail.getCode(),
+                    product.getProductId(),
+                    quantity,
+                    product.getPrice(),
+                    product.getDiscountPrice(),
+                    product.getProductName(),
+                    productDetail.getColor().getName(),
+                    productDetail.getSize().getName()
+            );
+            cartItemList.add(cartItem);
+        }
 
         return "redirect:/carts";
     }
+
+    @GetMapping("/delete")
+    public String delete(@RequestParam("prDetailCode") String prDetailCode,
+                         @ModelAttribute("CARTS") List<CartItem> cartItemList){
+        cartItemList.removeIf(cartItem -> Objects.equals(prDetailCode, cartItem.getCode()));
+        return "redirect:/carts";
+    }
+
+    @GetMapping("/update")
+    public void update(@RequestParam("prDetailCode") String prDetailCode,
+                         @RequestParam("quantity") int quantity,
+                         @ModelAttribute("CARTS") List<CartItem> cartItemList
+                         ){
+        for (CartItem cartItem: cartItemList){
+            if(cartItem.getCode() == prDetailCode){
+                cartItem.setQuantity(quantity);
+            }
+        }
+    }
+
 }
 
 
